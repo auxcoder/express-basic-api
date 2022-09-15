cb221c3f7d152c5fc3ec26d346d8c2779d6aeb24
Pavel Duran <auxcoder@gmail.com>
2022-09-14 21:24:31 -0400

    #20 Review email on register for account confirmation

--- types/common.ts
+++ types/common.ts
@@ -1,3 +1,7 @@
+import emailRepository from '../core/email';
+import {newUser} from '../routes/middleware/validateUser';
+import buildTemplateModel from '../utils/buildTemplateModel';
+
 export interface TLooseObj {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   [key: string]: any;
@@ -9,4 +13,18 @@ export type Profile = {
   email: string;
   iat?: string | null;
   uat?: string | null;
+}
+
+export type TemplateModel = {
+  product_name: string;
+  name: string;
+  username: string;
+  product_url: string;
+  action_path: string;
+  login_path: string;
+  support_email: string;
+  sender_name: string;
+  help_url: string;
+  company_name: string;
+  company_address: string;
 }
--- utils/buildTemplateModel.ts
+++ utils/buildTemplateModel.ts
@@ -1,20 +1,23 @@
 import constants from '../config/constants';
+import {TemplateModel} from '../types/common';
 
 type UserData = {
   username: string; email: string; verifyToken: string;
 }
 type ClientData = {
-  host: string; action_url: string; login_url: string
+  host: string;
+  action_path: string;
+  login_path: string
 }
 
-export default function buildTemplateModel(user: UserData, client: ClientData) {
+export default function buildTemplateModel(user: UserData, client: ClientData): TemplateModel {
   return {
     product_name: 'Prisma & Express',
     name: user.username,
     username: user.email,
     product_url: client.host,
-    action_url: `${client.host}${client.action_url}/${user.verifyToken}`,
-    login_url: `${client.host}${client.login_url}`,
+    action_path: `${client.host}${client.action_path}/${user.verifyToken}`,
+    login_path: `${client.host}${client.login_path}`,
     support_email: constants.emailData.supportEmail,
     sender_name: 'Support Team',
     help_url: `${client.host}$/help`,
